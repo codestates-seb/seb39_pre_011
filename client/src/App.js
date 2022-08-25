c78f35d9b74e91cc0f77ce56a7fa202bd6231d25
Beanxx <tnqls991211@naver.com>
2022-08-25 21:16:49 +0900

    fix: router 설정(+ outlet)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
-import Main from "./pages/Main";
+import Main from "./pages/Questions";
 import Nav from "./components/Nav";
 import Ask from "./pages/Ask";
 import Detail from "./pages/Detail";
 import MyPage from "./pages/MyPage";
 import Users from "./pages/Users";
+import Tags from "./pages/Tags";
+import SidebarLayout from "./components/SidebarLayout";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
+        <Nav />
         <AppContainer>
-          <Nav />
           <Routes>
-            <Route path="/" element={<Main />} />
+            <Route element={<SidebarLayout />}>
+              <Route path="/" element={<Main />} />
+              <Route path="/tags" element={<Tags />} />
+              <Route path="/users" element={<Users />} />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/ask" element={<Ask />} />
             <Route path="/detail" element={<Detail />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/tags" element={<tags />} />
-            <Route path="/users" element={<Users />} />
+            <Route path="/users:id" element={<MyPage />} />
           </Routes>
         </AppContainer>
       </Router>
@@ -32,7 +41,11 @@ export default App;
 
 const AppContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  padding: 1rem;
+  width: 100vw;
+  height: 100vh;
+  margin-top: 50px;
 `;
