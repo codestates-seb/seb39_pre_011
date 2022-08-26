00e11b99ccd8fc3ce2a85f946350cf9ba52c879f
mini <0miiii@naver.com>
2022-08-26 23:33:46 +0900

    design: css code delete

--- client/src/App.js
+++ client/src/App.js
@@ -41,10 +41,4 @@ export default App;
 const AppContainer = styled.div`
   position: relative;
   top: 50px;
-  /* display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh; */
 `;
