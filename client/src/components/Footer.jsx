a9758e9d3f61729b97df185a39df0e0290509096
Choi Soobin <64299610+Beanxx@users.noreply.github.com>
2022-08-28 21:14:44 +0900

    feat: footer 구현 (#26)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,73 +1,87 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import LogoSrc from "../assets/sprites.svg";
+import { ReactComponent as LogoImg } from "../assets/logo.svg";
+import Nav from "./Nav";
 
 function Footer() {
   return (
     <Container>
-      <NavLink to="/">
-        <Logo />
-      </NavLink>
-      <InnerBox>
-        <Title>STACK OVERFLOW</Title>
-        <ul>
-          <li>Questions</li>
-          <li>Help</li>
-        </ul>
-      </InnerBox>
-      <InnerBox>
-        <Title>PRODUCTS</Title>
-        <ul>
-          <li>Teams</li>
-          <li>Advertising</li>
-          <li>Collectives</li>
-          <li>Talent</li>
-        </ul>
-      </InnerBox>
-      <InnerBox>
-        <Title>COMPANY</Title>
-        <ul>
-          <li>About</li>
-          <li>Press</li>
-          <li>Work Here</li>
-          <li>Legal</li>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-          <li>Contact Us</li>
-          <li>Cookie Settings</li>
-          <li>Cookie Policy</li>
-        </ul>
-      </InnerBox>
-      <InnerBox>
-        <Title>STACK EXCHANGE NETWORK</Title>
-        <ul>
-          <li>Technology</li>
-          <li>Culture & recreation</li>
-          <li>Life & arts</li>
-          <li>Science</li>
-          <li>Professional</li>
-          <li>Business</li>
-          <li> </li>
-          <li>API</li>
-          <li>Data</li>
-        </ul>
-      </InnerBox>
-      <InnerBox>
-        <SnsBox>
-          <ul>
-            <li>Blog</li>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>LinkedIn</li>
-            <li>Instagram</li>
-          </ul>
-        </SnsBox>
-        <Copyright>
-          Site design / logo © 2022 Stack Exchange Inc; user contributions
-          licensed under CC BY-SA. rev 2022.8.26.42919
-        </Copyright>
-      </InnerBox>
+      <Box>
+        <LogoBox>
+          <NavLink to="/">
+            <LogoImg />
+          </NavLink>
+        </LogoBox>
+        <CenterBox>
+          <InnerBox>
+            <Title>STACK OVERFLOW</Title>
+            <ul>
+              <NavLink to="/">
+                <li>Questions</li>
+              </NavLink>
+              <li>Help</li>
+            </ul>
+          </InnerBox>
+          <InnerBox>
+            <Title>PRODUCTS</Title>
+            <ul>
+              <li>Teams</li>
+              <li>Advertising</li>
+              <li>Collectives</li>
+              <li>Talent</li>
+            </ul>
+          </InnerBox>
+          <InnerBox>
+            <Title>COMPANY</Title>
+            <ul>
+              <li>About</li>
+              <li>Press</li>
+              <li>Work Here</li>
+              <li>Legal</li>
+              <li>Privacy Policy</li>
+              <li>Terms of Service</li>
+              <li>Contact Us</li>
+              <li>Cookie Settings</li>
+              <li>Cookie Policy</li>
+            </ul>
+          </InnerBox>
+          <InnerBox>
+            <Title>STACK EXCHANGE NETWORK</Title>
+            <ul>
+              <li>Technology</li>
+              <li>Culture & recreation</li>
+              <li>Life & arts</li>
+              <li>Science</li>
+              <li>Professional</li>
+              <li>Business</li>
+              <li> </li>
+              <li>API</li>
+              <li>Data</li>
+            </ul>
+          </InnerBox>
+        </CenterBox>
+
+        <RightBox>
+          <SnsBox>
+            <ul>
+              <li>Blog</li>
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>LinkedIn</li>
+              <li>Instagram</li>
+            </ul>
+          </SnsBox>
+          <Copyright>
+            <p>
+              Site design / logo © 2022 Stack Exchange Inc; user contributions
+              licensed under <span>CC BY-SA.</span>
+            </p>
+
+            <p>rev 2022.8.26.42919</p>
+          </Copyright>
+        </RightBox>
+      </Box>
     </Container>
   );
 }
@@ -78,19 +92,32 @@ const Container = styled.footer`
   width: 100%;
   height: 322px;
   background-color: #232629;
-  padding: 1rem;
-  display: flex;
-  justify-content: center;
+  padding: 32px 12px 12px 12px;
 `;
 
-const Logo = styled.div`
-  background: url(${LogoSrc}) no-repeat 0 100%;
-  width: 32px;
-  height: 37px;
+const Box = styled.div`
+  width: 83%;
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+  margin: 0 auto;
 `;
 
 const InnerBox = styled.div`
-  margin: 1rem 3rem;
+  flex: 3;
+  li {
+    color: #9fa6ad;
+    line-height: 2;
+    font-size: 13px;
+  }
+`;
+
+const RightBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 1 1 150px;
+
   li {
     color: #9fa6ad;
     line-height: 1.6;
@@ -100,15 +127,39 @@ const InnerBox = styled.div`
 const Title = styled.h5`
   color: #babfc4;
   text-transform: uppercase;
-  font-weight: bold;
+  font-size: 13px;
+  font-weight: 700;
   margin-bottom: 12px;
 `;
 
 const SnsBox = styled.div`
   display: flex;
   flex-direction: column;
+  font-size: 11px;
+
+  ul li {
+    float: left;
+    margin-right: 1rem;
+  }
 `;
 
 const Copyright = styled.div`
   color: #9199a1;
+  font-size: 11px;
+  line-height: 14px;
+  padding-right: 3rem;
+  letter-spacing: 0.7px;
+  p span {
+    text-decoration: underline;
+  }
+`;
+
+const LogoBox = styled.div`
+  flex: 0 0 64px;
+`;
+
+const CenterBox = styled.div`
+  display: flex;
+  flex: 2 1 auto;
+  flex-direction: row;
 `;
